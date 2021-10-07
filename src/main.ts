83a5c411e2a1e9c590837094eb77c5f97f824a65
Jack Davenport <jack.davenport5011@gmail.com>
2021-10-07 23:32:28 +1100

    Added login and command handler code

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,12 @@ import { SlasherClient } from "discord.js-slasher";
 
 const client = new SlasherClient({ useAuth: true });
 
+client.on("command", (ctx) => {
+    ctx.reply(ctx.name);
+});
+
+client.on("ready", () => {
+    console.log("[Bot] Logged in: " + client.user.tag);
+});
+
+client.login();
